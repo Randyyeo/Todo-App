a8c9ef6dd649aec03bad97835f4e67cde007215a
Randall Yeo <randallysk@gmail.com>
2021-07-23 14:05:57 +0800

    Added env files

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase/app'
 import 'firebase'
 var firebaseConfig = {
-    apiKey: "AIzaSyDjRxxU7ErdOKCpP2Lay7IewA8Kp-bCtOU",
-    authDomain: "todo-70cc4.firebaseapp.com",
-    projectId: "todo-70cc4",
-    storageBucket: "todo-70cc4.appspot.com",
-    messagingSenderId: "79341932152",
-    appId: "1:79341932152:web:9ed6c7b169c43b9170678d",
-    measurementId: "G-EPJZHC7BCR"
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
   };
   var app = null
 
